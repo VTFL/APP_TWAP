d5e81f40d134281782f49a9a1a00db61fcf5f142
Florian Alline <florian.alline@etu.univ-lehavre.fr>
2017-03-03 16:31:55 +0100

    MAJ Lecture

--- raspberry/LectureArduino.java
+++ raspberry/LectureArduino.java
@@ -16,7 +16,7 @@ public class LectureArduino {
         Random r = new Random();
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+            //con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -26,7 +26,8 @@ public class LectureArduino {
         final GpioController gpio = GpioFactory.getInstance();
 
         // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
-        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+        //GPIO 24 = port 19 breadboard
+        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_25, PinPullResistance.PULL_DOWN);
 
         // set shutdown state for this input pin
         myButton.setShutdownOptions(true);
@@ -36,25 +37,27 @@ public class LectureArduino {
             @Override
             public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
                 // display pin state on console
-                System.out.println(" --> GPIO 2 PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
-                genererMontee(r, "Thibault Defortescu");
+                System.out.println(" --> GPIO 24 PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+                //genererMontee(r, "Thibault Defortescu");
             }
 
         });
 
-        final GpioPinDigitalInput myButton2 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00, PinPullResistance.PULL_DOWN);
+	//GPIO 25 = port 26 breadboard
+        final GpioPinDigitalInput myButton2 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_25, PinPullResistance.PULL_DOWN);
         myButton2.setShutdownOptions(true);
         myButton2.addListener(new GpioPinListenerDigital() {
             @Override
             public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                System.out.println(" --> GPIO 0 PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
-                genererMontee(r, "Florian Alline");
+                System.out.println(" --> GPIO 25 PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+                //genererMontee(r, "Florian Alline");
             }
 
         });
 
+	//GPIO 6 = port 25 breadboard
         // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
-        final GpioPinDigitalInput myButton3 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01, PinPullResistance.PULL_DOWN);
+        final GpioPinDigitalInput myButton3 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_06, PinPullResistance.PULL_DOWN);
 
         // set shutdown state for this input pin
         myButton3.setShutdownOptions(true);
@@ -64,8 +67,26 @@ public class LectureArduino {
             @Override
             public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
                 // display pin state on console
-                System.out.println(" --> GPIO 1 PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
-                genererMontee( r, "Lucas Patard");
+                System.out.println(" --> GPIO 6 PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+                //genererMontee( r, "Lucas Patard");
+            }
+
+        });
+        
+        //GPIO 29 = port 21 breadboard
+        // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
+        final GpioPinDigitalInput myButton4 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_29, PinPullResistance.PULL_DOWN);
+
+        // set shutdown state for this input pin
+        myButton4.setShutdownOptions(true);
+
+        // create and register gpio pin listener
+        myButton4.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                // display pin state on console
+                System.out.println(" --> GPIO 29 PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+                //genererMontee( r, "Valentin Pitel");
             }
 
         });
@@ -115,4 +136,3 @@ public class LectureArduino {
         }catch(Exception e){e.printStackTrace();}
     }
 }
-
